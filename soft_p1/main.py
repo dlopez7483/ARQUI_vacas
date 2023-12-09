40cf02e03b6d1efece79f3cf0cae5afb0f37e099
david <davidlopezxd860@gmail.com>
2023-12-08 21:24:37 -0600

    publicacion bt

--- soft_p1/main.py
+++ soft_p1/main.py
@@ -3,12 +3,14 @@
 import facebook
 import time
 temporal =""
-token_acceso = "EAADUTsiz24gBO1yRFWskwoFVkC7nkhUc4pzaJMAjjC1BEsmfGyTwmZB9J8DgAljO7tfXTqXGi6AmZBaJUZCTP1LNszfhsm9ZCntxlAcFdoF1uUSWIAxYLwsYp6fJJnqC4f6KJc2OS0cCYOFNISIR3JnyJfSI8TZCIOzaPFz7jAHRyJOVmQbid9TGlwzfJIPCcWFIv5fHrfyZATqY9oXjvxZCgfQOM1jlaAZD"
+token_acceso = "EAADUTsiz24gBOxKRUUnxSowRwkefbsNqvszVgkah5U5nId3VOzNBzEjqPGvi3DOGeVORs0Cl9rvvUchlxE1kNkN6iSQ3Ksuf7fKm7DZCMkSgugZBFZB8GG0ci7xAZCSK155u2brca2fBWvxcpr6Eq8Xpn65FpkZAqR7i6hLAx7ZACz6f6XsYBtx2lFYeg8qiskeWeKVmo1JmQyfUjZAKjgkE0iqkZCaH89oZD"
 #conexion a la api de facebook
 asfb = facebook.GraphAPI(access_token=token_acceso)
 
 
-
+cantidad_diablo=3
+cantidad_mantequilla=3
+cantidad_picnic=3
 
 
 #bucle para recibir informacion constantemente
@@ -19,13 +21,25 @@ while True:
     if temporal != publicaciones["data"][0]["message"]:
         #si la galleta es diablo responde 1
         if (publicaciones["data"][0]["message"] == "diablo"):
-            print(1)
+            if cantidad_diablo > 0:
+                print(1)
+                cantidad_diablo = cantidad_diablo - 1
+            else:
+              asfb.put_object(parent_object='me', connection_name='feed', message='No hay mas galletas diablo')
         #si la galleta es mantequilla responde 2
         elif (publicaciones["data"][0]["message"] == "mantequilla"):
-            print(2)
+            if cantidad_mantequilla > 0:
+                print(2)
+                cantidad_mantequilla = cantidad_mantequilla - 1
+            else:
+              asfb.put_object(parent_object='me', connection_name='feed', message='No hay mas galletas mantequilla')
         #si la galleta es picnic responde 3
         elif (publicaciones["data"][0]["message"] == "picnic"):
-            print(3)
+            if cantidad_picnic > 0:
+                print(3)
+                cantidad_picnic = cantidad_picnic - 1
+            else:
+              asfb.put_object(parent_object='me', connection_name='feed', message='No hay mas galletas picnic')
         #si no hay coincidencias imprime no hay coincidencias
         else:
          print("no hay coincidencias")
