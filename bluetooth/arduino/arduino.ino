4eebeece10d56ec453fb5be53f4c3d8ac1060d3a
jeach27 <3658279400101@ingenieria.usac.edu.gt>
2023-12-09 00:51:10 -0600

    casosReciboBluetooth
    -cambios en loop
    -switch 3 tipos de casos de galleta

--- bluetooth/arduino/arduino.ino
+++ bluetooth/arduino/arduino.ino
@@ -2,14 +2,44 @@ int ledPin = 13;
 int state = 0;
 
 void setup() {
-    pinMode(ledPin, OUTPUT);
-    digitalWrite(ledPin, LOW);
-    Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  Serial.begin(9600);
 }
- 
-void loop() {
 
-  if(Serial.available() > 0){
+void loop() {
+  switch (state) {
+    case 0:
+      {
+        if (Serial.available() > 0 && Serial.available() < 2) {
+          state = Serial.read();
+        }
+        if (state >= 1 && state <= 3) {
+          Serial.println("Numero aceptado");
+        } else {
+          Serial.println("Numero Invalido");
+          state = 0;
+        }
+      }
+    case 1:
+      {
+        //galleta tipo 1 diablo
+        state = 0;
+      }
+    case 2:
+      {
+        //galleta tipo 2 mantequilla
+        state = 0;
+      }
+    case 3:
+      {
+        //galleta tipo 3 picnic
+        state = 0;
+      }
+  }
+}
+
+/* if(Serial.available() > 0){
     state = Serial.read();
   } 
 
@@ -17,5 +47,4 @@ void loop() {
     digitalWrite(ledPin, HIGH);
     state = 0;
   }
-
-}
+*/
